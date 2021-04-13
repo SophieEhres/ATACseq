489a55a3b01195fd5b28b130f35929b058a602f2
SophieEhres <sophie.ehresmann@gmail.com>
2021-04-13 15:10:47 -0400

    move files to md5 directory after verification

--- checksums.sh
+++ checksums.sh
@@ -2,7 +2,9 @@
 2>&1
 date > log_checksums.txt
 
-dir="/Users/ehresms/computational/ATAC/FASTQ/readSetLinks"
+dir="/Users/ehresms/computational/ATAC/FASTQ/"
+md5_dir="${dir}"/md5
+mkdir -p ${md5_dir}
 
 for file in $(ls ${dir} | grep -v ".md5" | grep -e ".gz"); do
 	file_md5=$(md5 ${dir}/${file} | awk '{print $4}')
@@ -10,8 +12,11 @@ for file in $(ls ${dir} | grep -v ".md5" | grep -e ".gz"); do
 	
 	if [[ "${file_md5}" == "${md5}" ]] ; then
 		echo "${file} checked and ok"
+		mv ${dir}/${file}.md5 ${md5_dir}
 	else 
 		echo "${file} checked and not ok \\n file_md5 is ${file_md5} and md5 is ${md5}"
 	fi
 done >> log_checksums.txt
+
+
 
