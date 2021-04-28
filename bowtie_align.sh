d45e6a401e9b90018f55e76354bc4347c610b094
SophieEhres <sophie.ehresmann@gmail.com>
2021-04-28 15:59:20 -0400

    modified for new index and log file

--- bowtie_align.sh
+++ bowtie_align.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-2>&1
-date>log_trim.txt
+date>log_align.txt
 
 
 computational="/Users/ehresms/computational"
-genome="${computational}/genomes/human/bowtie2"
+genome="${computational}/genomes/human/bowtie2/GRCh38_noalt_as/GRCh38_noalt_as"
 trim_dir="${computational}/ATAC/trim"
 align_dir="${computational}/ATAC/align"
 
 mkdir -p ${align_dir}
 
-samples=$(ls ${trim_dir} | cut -d "_" -f1 | uniq | head -n 1)
+samples=$(ls ${trim_dir} | cut -d "_" -f1 | uniq )
 echo "samples are ${samples}"
 
 for sample in ${samples}; do
@@ -27,11 +26,11 @@ gunzip -k ${file_2}
 
 bowtie2 -x ${genome} -1 ${file_f} -2 ${file_r} \
 --phred33 --very-sensitive -X 1000 --no-discordant \
--t --threads 50 -S ${align_dir}/${sample}_aligned.sam
+-t --threads 30 -S ${align_dir}/${sample}_aligned.sam 2>>log_align.txt
 
 rm ${file_f}
 rm ${file_r}
 echo "done aligning ${sample}"
 
-done >>log_trim.txt
+done
 
