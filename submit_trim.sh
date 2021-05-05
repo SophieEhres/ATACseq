3b11ec0d4e30aee60815b700b3fe965af400f4ff
SophieEhres <p1044860@beluga5.int.ets1.calculquebec.ca>
2021-05-05 15:21:53 -0400

    added options <fastq_dir> <trim_dir>

--- submit_trim.sh
+++ submit_trim.sh
@@ -1,14 +1,16 @@
- #!/bin/bash
+#!/bin/bash
 
-fastq_dir="${SCRATCH}/ATAC/fastq"
-trim_dir="${SCRATCH}/ATAC/trim"
+fastq_dir="${1}"
+trim_dir="${2}"
 tool_dir="${SCRATCH}/tools"
 trimmomatic="${tool_dir}/Trimmomatic-0.39/trimmomatic-0.39.jar"
 adapters="${tool_dir}/Trimmomatic-0.39/adapters"
+trim_job="${SCRATCH}/scripts/RNAseq/trim_jobs"
 
 mkdir -p ${trim_dir}
+mkdir -p ${trim_job}
 
-samples=$(ls ${fastq_dir} | rev | cut -d "_" -f2 | rev | uniq )
+samples=$(ls ${fastq_dir}/*.gz | rev | cut -d "/" -f1 | cut -d "_" -f2 | rev | uniq )
 
 
 for sample in ${samples}; do 
@@ -29,8 +31,8 @@ module load java
 java -jar ${trimmomatic} PE -threads 20 -phred33 ${files} \
 ${trim_dir}/${sample}_forward_paired.fq.gz ${trim_dir}/${sample}_forward_unpaired.fq.gz \
 ${trim_dir}/${sample}_reverse_paired.fq.gz ${trim_dir}/${sample}_reverse_unpaired.fq.gz \
-ILLUMINACLIP:${adapters}/IlluminaNextera_PE.fa:2:30:10
-"> ${sample}_trimjob.sh 
-sbatch --account=def-sauvagm ${sample}_trimjob.sh 
+ILLUMINACLIP:${adapters}/Illumina_Nextera_PE.fa:2:30:10
+"> ${trim_job}/${sample}_trimjob.sh 
+#sbatch --account=def-sauvagm ${sample}_trimjob.sh 
 
 done
