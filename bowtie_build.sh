786f42704a94588238b5065b780449573f5ad1e8
SophieEhres <p1044860@beluga2.int.ets1.calculquebec.ca>
2021-05-06 14:53:27 -0400

    fixed input fasta

--- bowtie_build.sh
+++ bowtie_build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 now=$(date +"%Y-%m-%d-%H-%M")
 echo "${now}" > bowtiebuild_${now}.log
+bowtie2 --version >> bowtiebuild_${now}.log
 
 genome="${SCRATCH}/genomes/human/hg38/fasta"
 outdir="${SCRATCH}/genomes/human/hg38/bowtie_index"
 
-fastafiles=$(ls ${genome}/*.fa)
+mkdir -p ${outdir}
 
-bowtie2-build ${fastafiles} ${outdir} 2 > bowtiebuild_${now}.log
+fastafiles=$(ls ${genome}/*.fa | tr '\n' ',')
+echo "fasta files are $fastafiles"
+
+bowtie2-build ${fastafiles} ${outdir}/hg38_ --threads 10 2 >> bowtiebuild_${now}.log
