180895843f434afa61ebc058f64b5ba80b82e9ac
SophieEhres <p1044860@beluga5.int.ets1.calculquebec.ca>
2021-05-08 15:24:56 -0400

    modified to add SBATCH conditions

--- bowtie_build.sh
+++ bowtie_build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#SBATCH --time=6:00:00
+#SBATCH --job-name=bowtie_build
+#SBATCH --output=bowtie_build.out
+#SBATCH --error bowtie_build.err
+#SBATCH --ntasks=20
+#SBATCH --mem-per-cpu=4000
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=sophie.ehresmann@gmail.com
+#SBATCH --account=def-sauvagm
+
 now=$(date +"%Y-%m-%d-%H-%M")
 echo "${now}" > bowtiebuild_${now}.log
 bowtie2 --version >> bowtiebuild_${now}.log
@@ -11,4 +21,4 @@ mkdir -p ${outdir}
 fastafiles=$(ls ${genome}/*.fa | tr '\n' ',')
 echo "fasta files are $fastafiles"
 
-bowtie2-build ${fastafiles} ${outdir}/hg38_ --threads 10 2 >> bowtiebuild_${now}.log
+bowtie2-build ${fastafiles} ${outdir}/hg38_ --threads 20 2 >> bowtiebuild_${now}.log
