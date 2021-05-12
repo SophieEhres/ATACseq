bd2f2491fde4ad64eee53ca9d4fb35649bdfc7ca
SophieEhres <p1044860@beluga1.int.ets1.calculquebec.ca>
2021-05-12 19:09:49 -0400

    modified name error

--- get_mitoreads.sh
+++ get_mitoreads.sh
@@ -12,30 +12,26 @@ files=$(ls ${align_dir}/*.sam)
 
 for file in ${files}; do
 
-    name=$(echo "${file}"| xargs basename | cut -d "_" -f1)
-    
+name=$(echo "${file}"| xargs basename | cut -d "_" -f1) 
+
 echo "#!/bin/bash
 #SBATCH --time=1:00:00
-#SBATCH --job-name=getmitoreads
-#SBATCH --output=getmitoreads.log
+#SBATCH --job-name=${name}_getmitoreads
+#SBATCH --output=${outdir}/${name}_getmitoreads.log
 #SBATCH --ntasks=30
 #SBATCH --mem-per-cpu=4000
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=sophie.ehresmann@gmail.com
 #SBATCH --account=def-sauvagm
     
-    echo "$(date +"%Y-%m-%d-%H-%M")" >> ${outdir}/${sample}_getmitoreads.log
-    samtools --version >> ${outdir}/${sample}_getmitoreads.log
-    mitoreads=$(samtools view -h ${file} | grep -e "chrM" | wc -l)
-    totalreads=$(samtools view -h ${file} | wc -l)
+echo "$(date +"%Y-%m-%d-%H-%M")" >> ${outdir}/${name}_getmitoreads.log
+samtools --version >> ${outdir}/${name}_getmitoreads.log
+samtools view -h ${file} | grep -e "chrM" | wc -l > mitoreads
+samtools view -h ${file} | wc -l > totalreads
 
-    sum=$(expr ${mitoreads} \* 100)
-    percentage=$(expr ${sum} \/ ${totalreads})
+echo "For name ${name}, there are ${mitoreads} mitochondrial reads for a total of ${totalreads}" >> ${outdir}/${name}_getmitoreads.log
 
-    echo "For sample ${name}, there are ${mitoreads} mitochondrial reads for a total of ${totalreads}, accounting for ${percentage} % of reads" >> ${outdir}/${sample}_getmitoreads.log
-
-    samtools view -@ 30 -h ${file} | grep -v "chrM" | samtools sort -@ 30 -o ${sorted_dir}/${name}_sorted-rmChrM.bam -O bam 2>> ${outdir}/${sample}_getmitoreads.log
-
+samtools view -@ 30 -h ${file} | grep -v "chrM" | samtools sort -@ 30 -o ${sorted_dir}/${name}_sorted-rmChrM.bam -O bam 2>> ${outdir}/${name}_getmitoreads.log
 " > ${jobdir}/${name}_getmitoreads.sh
 
 sbatch ${jobdir}/${name}_getmitoreads.sh
