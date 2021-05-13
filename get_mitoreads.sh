30e02d2bc71924ac87f62a0e855d3c075e6409a7
SophieEhres <p1044860@beluga1.int.ets1.calculquebec.ca>
2021-05-12 23:04:01 -0400

    still have memory issues to fix

--- get_mitoreads.sh
+++ get_mitoreads.sh
@@ -7,6 +7,7 @@ outdir="${SCRATCH}/scripts/ATACseq/out_files/mitoreads"
 
 mkdir -p ${sorted_dir}
 mkdir -p ${jobdir}
+mkdir -p ${outdir}
 
 files=$(ls ${align_dir}/*.sam)
 
@@ -15,23 +16,22 @@ for file in ${files}; do
 name=$(echo "${file}"| xargs basename | cut -d "_" -f1) 
 
 echo "#!/bin/bash
-#SBATCH --time=1:00:00
+#SBATCH --time=3:00:00
 #SBATCH --job-name=${name}_getmitoreads
 #SBATCH --output=${outdir}/${name}_getmitoreads.log
-#SBATCH --ntasks=30
-#SBATCH --mem-per-cpu=4000
+#SBATCH --ntasks=20
+#SBATCH --mem-per-cpu=6000
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=sophie.ehresmann@gmail.com
 #SBATCH --account=def-sauvagm
     
 echo "$(date +"%Y-%m-%d-%H-%M")" >> ${outdir}/${name}_getmitoreads.log
 samtools --version >> ${outdir}/${name}_getmitoreads.log
-samtools view -h ${file} | grep -e "chrM" | wc -l > mitoreads
-samtools view -h ${file} | wc -l > totalreads
 
-echo "For name ${name}, there are ${mitoreads} mitochondrial reads for a total of ${totalreads}" >> ${outdir}/${name}_getmitoreads.log
+samtools view -h ${file} | grep -e "chrM" | wc -l > ${name}_mitoreads.txt
+samtools view -h ${file} | wc -l >> ${name}_mitoreads.txt
 
-samtools view -@ 30 -h ${file} | grep -v "chrM" | samtools sort -@ 30 -o ${sorted_dir}/${name}_sorted-rmChrM.bam -O bam 2>> ${outdir}/${name}_getmitoreads.log
+samtools view -@ 20 -h ${file} | grep -v "chrM" | samtools sort -@ 20 -o ${sorted_dir}/${name}_sorted-rmChrM.bam -O bam 2>> ${outdir}/${name}_getmitoreads.log
 " > ${jobdir}/${name}_getmitoreads.sh
 
 sbatch ${jobdir}/${name}_getmitoreads.sh
