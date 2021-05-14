4bd8445a81774f97be3971adc1bfa8f322da96c5
SophieEhres <p1044860@beluga5.int.ets1.calculquebec.ca>
2021-05-13 21:08:19 -0400

    added check mito file

--- get_mitoreads.sh
+++ get_mitoreads.sh
@@ -13,14 +13,18 @@ files=$(ls ${align_dir}/*.sam)
 
 for file in ${files}; do
 
-name=$(echo "${file}"| xargs basename | cut -d "_" -f1) 
+    name=$(echo "${file}"| xargs basename | cut -d "_" -f1) 
+
+    if [ -f ${sorted_dir}/${name}_sorted-rmChrM.bam ]; then
+        echo "${name} already mito read"
+    else
 
 echo "#!/bin/bash
-#SBATCH --time=3:00:00
+#SBATCH --time=6:00:00
 #SBATCH --job-name=${name}_getmitoreads
 #SBATCH --output=${outdir}/${name}_getmitoreads.log
 #SBATCH --ntasks=20
-#SBATCH --mem-per-cpu=6000
+#SBATCH --mem-per-cpu=8000
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-user=sophie.ehresmann@gmail.com
 #SBATCH --account=def-sauvagm
@@ -35,5 +39,8 @@ samtools view -@ 20 -h ${file} | grep -v "chrM" | samtools sort -@ 20 -o ${sorte
 samtools index -@ 20 ${sorted_dir}/${name}_sorted-rmChrM.bam ${sorted_dir}/${name}_sorted-rmChrM.bai
 " > ${jobdir}/${name}_getmitoreads.sh
 
-sbatch ${jobdir}/${name}_getmitoreads.sh
+    sbatch ${jobdir}/${name}_getmitoreads.sh
+
+    fi
+
 done
