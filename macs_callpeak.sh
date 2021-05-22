942c0ac5fa782986c7952aaf09198682e2539fc0
SophieEhres <p1044860@beluga2.int.ets1.calculquebec.ca>
2021-05-21 21:32:41 -0400

    callpeaks using macs2

--- macs_callpeak.sh
+++ macs_callpeak.sh
@@ -11,7 +11,7 @@ mkdir -p ${jobdir}
 mkdir -p ${peakdir}
 
 
-for name in $(ls ${cleandir} | rev | cut -d "_" -f2- | rev | head -n 1); do
+for name in $(ls ${cleandir} | rev | cut -d "_" -f2- | rev); do
 
  file=${cleandir}/${name}_clean.bam
 
